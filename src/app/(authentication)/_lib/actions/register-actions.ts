a5b2bafb2d2bd3fed37aa94dd9011d3745d42683
churchilldotexe <churchillexe@gmail.com>
2024-07-19 14:59:56 +0800

    chore: connect registration form to database

--- src/app/(authentication)/_lib/actions/register-actions.ts
+++ src/app/(authentication)/_lib/actions/register-actions.ts
@@ -1,8 +1,12 @@
 "use server";
 
+import { createUser } from "@/server/data-access/authentication";
 import { registerUserFormSchema } from "../schema";
 
-type registerFormReturn = { message?: string };
+type registerFormReturn = {
+  message?: string;
+  error?: Record<string, string[]>;
+};
 
 export async function registerFormActions(
   initialState: registerFormReturn,
@@ -12,8 +16,11 @@ export async function registerFormActions(
   const parsedFormData = registerUserFormSchema.safeParse(rawFormData);
   if (parsedFormData.success === false) {
     console.log(parsedFormData.error.errors);
+    return { error: parsedFormData.error.formErrors.fieldErrors };
   }
-  console.log(parsedFormData.data);
-  // NOTE: logic here
+
+  const { displayName, email, userName, password } = parsedFormData.data;
+  await createUser({ displayName, email, userName, hashedPassword: password });
+  console.log("user creation success");
   return { message: "all good" };
 }
--- src/server/data-access/authentication.ts
+++ src/server/data-access/authentication.ts
@@ -2,6 +2,17 @@ import "server-only";
 import { ZodError } from "zod";
 import { type CreateUserTypes, createUserSchema } from "../db/schema/users";
 import { turso } from "../turso";
+import crypto, { randomUUID } from "node:crypto";
+
+// for better password protection
+const generateSalt = () => {
+  return crypto.randomBytes(16).toString("hex");
+};
+
+// for User security
+const generateHashPassword = (password: string, salt: string) => {
+  return crypto.pbkdf2Sync(password, salt, 10000, 64, "sha512").toString("hex");
+};
 
 export async function createUser(userInfo: CreateUserTypes) {
   const parsedUserInfo = createUserSchema.safeParse(userInfo);
@@ -9,12 +20,24 @@ export async function createUser(userInfo: CreateUserTypes) {
     throw new ZodError(parsedUserInfo.error.errors);
   }
 
+  console.log("parsed success");
+
+  const { hashedPassword: password, userName, email, displayName } = parsedUserInfo.data;
+
+  const salt = generateSalt();
+  console.log("salt", salt);
+  const hashedPassword = generateHashPassword(password, salt);
+  console.log("hasedpw", hashedPassword);
+  const id = randomUUID();
+
   await turso.execute({
     sql: `INSERT INTO
             users
-              (user_name, email, display_name, hashed_password, refresh_token, token_fingerprint, id)
+              (user_name, email, display_name, hashed_password,id)
           VALUES
-              (:username, :email, :displayName, :hashedPassword, :refreshToken, :tokenFingerprint, :id)`,
-    args: parsedUserInfo.data,
+              (:userName, :email, :displayName, :hashedPassword, :id)`,
+    args: { userName, email, displayName, hashedPassword, id },
   });
+
+  console.log("able to create user in db");
 }
