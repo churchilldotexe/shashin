19a6cd942c4190a180ab5a91113b592bec7a8d49
churchilldotexe <churchillexe@gmail.com>
2024-07-28 19:50:52 +0800

    refactor: moved postbutton component to lessen reusability

---
+++ src/components/ui/PostButton.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils/cn";
+import { Loader2 } from "lucide-react";
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+import { useFormStatus } from "react-dom";
+
+// (property) JSX.IntrinsicElements.button: DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+export function PostButton({
+  children,
+  className,
+  ...props
+}: { children: ReactNode } & ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className={cn(
+        "rounded-md bg-primary px-2 py-1 text-primary-foreground",
+        { "bg-muted": pending },
+        className
+      )}
+      type="submit"
+      disabled={pending}
+      {...props}
+    >
+      {pending ? <Loader2 className="m-auto animate-spin text-center" /> : children}
+    </button>
+  );
+}
