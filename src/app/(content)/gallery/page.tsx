812f2e2f670862c7f644d2e5f85639c3f22b91d4
churchillexe@gmail.com <tingexe@DESKTOP-DC002H1.>
2024-08-12 01:06:25 +0800

    chore: redirect to album sub folder then redirect to gallery

--- src/app/(content)/gallery/page.tsx
+++ src/app/(content)/gallery/page.tsx
@@ -1,45 +1,5 @@
-import Link from "next/link";
-import type { CSSProperties } from "react";
+import { type RedirectType, redirect } from "next/navigation";
 
 export default function GalleryPage() {
-  return (
-    <div>
-      <Link href={"/img/1"} passHref>
-        <div style={{ "--i": "1" } as CSSProperties} className="fade-in-image">
-          GalleryPage 1
-        </div>
-      </Link>
-      <Link href={"/img/2"}>
-        <div style={{ "--i": "2" } as CSSProperties} className="fade-in-image">
-          GalleryPage 2
-        </div>
-      </Link>
-      <Link href={"/img/3"}>
-        <div style={{ "--i": "3" } as CSSProperties} className="fade-in-image">
-          GalleryPage 3
-        </div>
-      </Link>
-      <Link href={"/img/4"}>
-        <div style={{ "--i": "4" } as CSSProperties} className="fade-in-image">
-          GalleryPage 4
-        </div>
-      </Link>
-      <Link href={"/img/5"}>
-        <div style={{ "--i": "5" } as CSSProperties} className="fade-in-image">
-          GalleryPage 5
-        </div>
-      </Link>
-      <Link href={"/img/5"}>
-        <div style={{ "--i": "6" } as CSSProperties} className="fade-in-image">
-          GalleryPage 6
-        </div>
-      </Link>
-
-      <div id="imANode" />
-    </div>
-  );
+  redirect("/gallery/albums", "replace" as RedirectType);
 }
-
-// NOTE: will show here all the stats of with a clickable that will direct them to albums, images, favorites
-// stats includes: Counts , rate of posts(eg like a graph for the amount of the post in the past month/weeks)
-// Or rewrite// will redirect to albums
