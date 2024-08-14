4ed3eefbbc6531a17659dae83aefb4acaf67f0c9
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-08-14 21:24:16 +0800

    refactor: toggle instead of dropdown

--- src/components/ui/DisplayModeToggle.tsx
+++ src/components/ui/DisplayModeToggle.tsx
@@ -1,87 +1,28 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { type LucideProps, Moon, Sun, SunMoon } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useState } from "react";
-
-function DisplayMode({
-  mode,
-  className,
-  ...props
-}: {
-  mode?: string | undefined;
-  className?: string;
-  props?: LucideProps;
-}) {
-  switch (mode) {
-    case "light":
-      return <Sun className={cn("size-5", className)} {...props} />;
-    case "dark":
-      return <Moon className={cn("size-5", className)} {...props} />;
-    case "system":
-      return <SunMoon className={cn("size-5", className)} {...props} />;
-  }
-}
 
 export function DisplayModeDropDown() {
-  const { setTheme } = useTheme();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [mode, setMode] = useState<"dark" | "light" | "system">("system");
-
-  const handleDropDownToggle = (newMode: "dark" | "light" | "system") => {
-    setIsOpen(false);
-    setTheme(newMode);
-    setMode(newMode);
-  };
+  const { theme, setTheme } = useTheme();
 
   return (
-    <details
-      className="custom-overlay relative bg-background"
-      open={isOpen}
-      onToggle={(e) => {
-        setIsOpen(e.currentTarget.open);
-      }}
-    >
-      <summary className="list-none">
-        <div className="cursor-pointer">
-          <DisplayMode mode={mode} />
-        </div>
-      </summary>
-      <div
-        className={cn(
-          " -translate-x-1/2 absolute left-[-150%] z-[1000] flex flex-col divide-y divide-muted rounded border bg-background text-secondary-foreground transition-all duration-300 ease-in-out",
-          isOpen ? "scale-100 opacity-100" : "scale-90 opacity-0"
-        )}
-      >
-        <button
-          className="flex items-center gap-2 hocus-visible:bg-secondary p-3"
-          onClick={() => {
-            handleDropDownToggle("light");
-          }}
-          type="button"
-        >
-          <Sun className="size-4" /> Light
-        </button>
-        <button
-          className="flex items-center gap-2 hocus-visible:bg-secondary p-3"
-          onClick={() => {
-            handleDropDownToggle("dark");
-          }}
-          type="button"
-        >
-          <Moon className="size-4" /> Dark
-        </button>
-        <button
-          className="flex items-center gap-2 hocus-visible:bg-secondary p-3"
-          onClick={() => {
-            handleDropDownToggle("system");
-          }}
-          type="button"
-        >
-          <SunMoon className="size-4" /> System
-        </button>
+    <label className="grid cursor-pointer">
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          const isChecked = e.target.checked;
+          isChecked ? setTheme("light") : setTheme("dark");
+        }}
+        checked={theme === "light"}
+        className="peer/theme sr-only"
+      />
+      <div className=" block transition-all ease-in-out peer-checked/theme:hidden ">
+        <Moon />
       </div>
-    </details>
+      <div className=" hidden transition-all ease-in-out peer-checked/theme:block ">
+        <Sun />
+      </div>
+    </label>
   );
 }
