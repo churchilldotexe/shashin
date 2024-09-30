756f5ab5b3eea78e97ccfa314cf4350447e531b6
churchillexe@gmail.com <tingexe@DESKTOP-7L9OA58.>
2024-09-30 23:44:21 +0800

    style: loading component svg animation

--- src/app/(authentication)/loading.tsx
+++ src/app/(authentication)/loading.tsx
@@ -2,8 +2,46 @@ import { PageSection } from "@/components/PageSection";
 
 export default function Loading() {
   return (
-    <PageSection className="min-h-[100dvh]">
-      <div className="loader" />
+    <PageSection className="flex min-h-[100dvh] items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        width="300"
+        height="300"
+        aria-labelledby="loadingTitle loadingDesc"
+        role="img"
+      >
+        <title id="loadingTitle">Loading animation</title>
+        <desc id="loadingDesc">An animated logo indicating that content is loading</desc>
+        <path
+          className="path"
+          d="M 40 23 a 0 0 0 0 0 0 -3 h -25 v 40 h 25 a 0 0 0 0 0 0 -3 h -20 v -20 h 16 a 0 0 0 0 0 0 -3 h -16 v -11 z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth="1"
+        />
+        <path
+          className="path"
+          d="M 60 23 a 0 0 0 0 0 0 -3 h 25 v 40 h -25 a 0 0 0 0 0 0 -3 h 20 v -20 h -16 a 0 0 0 0 0 0 -3 h 16 v -11 z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth="1"
+        />
+        <path
+          className="path"
+          d="M 34 10 l 35 60 a 0 0 0 0 0 -3 0 l -35 -60 z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth="1"
+        />
+        <path
+          className="path"
+          d="M 66 10 l -35 60 a 0 0 0 0 0 3 0 l 35 -60 z"
+          stroke="currentColor"
+          fill="none"
+          strokeWidth="1"
+        />
+      </svg>
     </PageSection>
   );
 }
